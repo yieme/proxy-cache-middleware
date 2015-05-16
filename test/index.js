b18b82c0537e0f01091cf006c7f2ae4e6953004d
yieme <yieme@cormea.com>
2015-05-16 12:39:21 -0600

    rework

--- test/index.js
+++ test/index.js
@@ -1,14 +1,8 @@
-var should = require('chai').should(),
-    proxyCacheMiddleware = require('..')
-;
+var should = require('chai').should()
 
 describe('proxy-cache-middleware', function() {
-  var expected = ["hello", "world"]
-  var expectedString = JSON.stringify(expected)
-  it('should eaual ' + expectedString, function(done) {
-    var test = proxyCacheMiddleware()
-    var json = JSON.stringify(test)
-    json.should.equal(expectedString);
+  it('should load', function(done) {
+    require('..')
     done();
   });
 });
